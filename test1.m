f15f5c51f564e59b69ac8c5f13543e748ee6cd21
Shohei Aoki <shoaok@gmail.com>
2016-01-19 20:23:29 +0900

    Fix

--- test1.m
+++ test1.m
@@ -13,39 +13,37 @@ x2=L1*cos(theta1)+L2*cos(theta1+theta2);
 y2=L1*sin(theta1)+L2*sin(theta1+theta2);
 x=[0 x1 x2];
 y=[0 y1 y2];
-plot(x,y);
-plot(x,y,'o')
 
+plot(x,y,'b-','LineWidth',2.5);
+plot(x,y,'bo')
 
-%????????Theta?????????
-thetas=linspace(0,theta1,21);
-theta0=0;
-r0=0;
-a=0.5;
-for idx=1:length(thetas)
-    theta=thetas(idx);
-    r(idx)= r0*cos(theta-theta0)+sqrt((r0*cos(theta-theta0))^2-(r0^2-a^2));      
-end
-polar(thetas,r);
-
+drawAngle(0,0,theta1);
+drawAngle(L1,theta1,theta2);
 
 %?P1??????Theta2???OP1?????
-r0=L1;
-a=0.5;
-theta0=theta1;
-x=sqrt((L1^2+a^2)-2*L1*a*cos(pi-theta2));
-theta3=acos((L1^2+x^2-a^2)/(2*L1*x));
-thetas=linspace(theta1,theta1+theta3,21);
-for idx=1:length(thetas)
-    theta=thetas(idx);
-    r(idx)= r0*cos(theta-theta0)+sqrt((r0*cos(theta-theta0))^2-(r0^2-a^2));      
-end
-xslice=a*3;
-ybound=y1/x1*xslice;
-plot([x1 xslice],[y1 ybound],'--');
-polar(thetas,r);
 
 
+    function drawAngle(r0,th1,th2)
+        a=0.5;
+        th0=th1;
+        x=sqrt((L1^2+a^2)-2*L1*a*cos(pi-th2));
+        th3=acos((L1^2+x^2-a^2)/(2*L1*x));
+        
+        if r0~=0
+            thetas=linspace(th1,th1+th3,21);
+        else
+            thetas=linspace(th1,th2,21);
+        end
+        
+        for idx=1:length(thetas)
+            theta=thetas(idx);
+            r(idx)= r0*cos(theta-th0)+sqrt((r0*cos(theta-th0))^2-(r0^2-a^2));
+        end
+        xslice=a*3;
+        ybound=y1/x1*xslice;
+        plot([x1 xslice],[y1 ybound],'k--');
+        polar(thetas,r,'r-');                        
+    end
 
 
 end
