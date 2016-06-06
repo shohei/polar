aba9dc2b18f52ecfc356f11600c3694170a15293
Shohei Aoki <shoaok@gmail.com>
2016-06-06 16:52:50 +0900

    add v computation

--- simulator/draw.m
+++ simulator/draw.m
@@ -11,42 +11,81 @@ r = x.^2+y.^2;
 theta = atan2(y,x);
 rmax = max(r);
 thetamax = max(theta);
+rdot(1) = 0;
+thetadot(1) = 0;
+dt = 0.02;
+for idx=2:sample
+    rdot(idx) = (r(idx)-r(idx-1))/dt;
+    thetadot(idx) = (theta(idx)-theta(idx-1))/dt;
+end
+rdotmax = max(rdot);
+thetadotmax = max(thetadot);
 
-% drawRandTheta();
+v = r.*thetadot;
+vmax = max(v);
 
 FigHandle = figure;
-set(FigHandle, 'Position', [100, 100, 900, 400]);
+set(FigHandle, 'Position', [100, 100, 1200, 500]);
 
-subplot(2,2,[1 3]);
+subplot(3,3,[1 4]);
 title('Destination');
 hold on;
 xlim([0,alpha]);
 ylim([0,alpha]);
 
-subplot(222);
+subplot(332);
 title('r');
 hold on;
 xlim([0,sample]);
 ylim([0,rmax]);
 
-subplot(224);
+subplot(335);
 title('$$\theta$$','interpreter','latex');
 hold on;
 xlim([0,sample]);
 ylim([0,thetamax]);
 
+subplot(333);
+title('$$\dot{r}$$','interpreter','latex');
+hold on;
+xlim([0,sample]);
+ylim([0,rdotmax]);
+
+subplot(336);
+title('$$\dot{\theta}$$','interpreter','latex');
+hold on;
+xlim([0,sample]);
+ylim([0,thetadotmax]);
+
+subplot(338);
+title('v');
+hold on;
+xlim([0,sample]);
+ylim([0,vmax]);
+
+
 for idx=1:sample
-    subplot(2,2,[1 3]);
+    subplot(3,3,[1 4]);
     plot(x(idx),y(idx),'ro');
+    plot(x(1:idx),y(1:idx),'b-','LineWidth',0.01);
     
-    subplot(222);
+    subplot(332);
     plot(r(1:idx),'b-','LineWidth',1)
     
-    subplot(224);
+    subplot(335);
     plot(theta(1:idx),'b-','LineWidth',1);
     
+    subplot(333);
+    plot(rdot(1:idx),'b-','LineWidth',1)
+    
+    subplot(336);
+    plot(thetadot(1:idx),'b-','LineWidth',1);    
+    
+    subplot(338);
+    plot(v(1:idx),'b-','LineWidth',1);    
+
     drawnow();
-    pause(0.02);
+    pause(dt);
 end
 
 
