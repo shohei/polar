7b69eeba7d03b83462bc0d1b52f69fe43f4f5359
Shohei Aoki <shoaok@gmail.com>
2016-06-08 18:47:51 +0900

    add gif image

--- simulator/compare_velo.m
+++ simulator/compare_velo.m
@@ -17,15 +17,14 @@ set(FigHandle, 'Position', [100, 100, 600, 600]);
 
 subplot(111);
 axis equal;
-title('Line planning by Bresenham algorithm');
+title('Destination');
 xlim([0,32]);
 ylim([0,32]);
 hold on;
 grid on;
 grid minor;
 
-% writerObj = VideoWriter('newfile.avi');
-% open(writerObj);
+
 
 for idx=1:sample
     subplot(1,1,1);
@@ -33,10 +32,6 @@ for idx=1:sample
     plot(x(1:idx),y(1:idx),'b-');
  
     drawnow();
-
-%     frame = getframe(gcf);
-%     writeVideo(writerObj, frame);
-
 end
 
 
@@ -45,24 +40,21 @@ if(deltaX>deltaY)
  nextX=x(1);
  nextY=y(1);
  while (nextX ~= x(2)) 
+%           fraction>=0????Q(a+i+1,b+j+1)???
+%           ????nextX,nextY???+1
             if (D >= 0) 
-                nextY = nextY + 1;      
-                D = D - deltaX;  
+                nextY = nextY + 1;      %y?1????
+                D = D - deltaX;  %fraction?deltaX??            
             end
-            nextX = nextX + 1; 
-            D = D + deltaY;  
+%           fraction<0????P(a+i+1,b+j)???
+%           ????nextX??+1
+            nextX = nextX + 1;      %// x?1????
+            D = D + deltaY;  %// fraction?deltaY???
 
             subplot(1,1,1);
             plot(nextX, nextY,'r*');
             drawnow();
-
-%             frame = getframe(gcf);
-%             writeVideo(writerObj, frame);
-
-            pause(0.1);
+            pause(0.5);
  end
 
-% close(writerObj);
-
- 
 end
