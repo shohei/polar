b1889441ea669aac0bedd413dbaf8c633e0cc988
Shohei Aoki <shoaok@gmail.com>
2016-06-14 19:20:10 +0900

    update note

--- jerk/acceleration.m
+++ jerk/acceleration.m
@@ -13,13 +13,32 @@ r=[0];
 for idx=1:length(t)-1
   r(idx+1) = r(idx) + rdot(idx)*(t(idx+1)-t(idx));
 end
-
-plot(t,r,'r');
-title('r','interpreter','latex');
-return;
-
 thetadot = (5*t).*(t<=2) + 10*(t>2&t<8) + (-5*t+50).*(t>=8);
 v = sqrt(rdot.^2+(r.^2).*thetadot.^2);
+
+s=[0];
+for idx=1:length(t)-1
+  s(idx+1) = s(idx) + v(idx)*(t(idx+1)-t(idx));
+end
+
+subplot(121);
+plot(t,r,'r');
+title('r','interpreter','latex');
+
+subplot(122);
+plot(t,s,'b');
+title('s','interpreter','latex');
+
+set(gcf,'color','w');
+fig = gcf;
+fig.PaperUnits = 'inches';
+fig.PaperPosition = [0 0 6 3];
+fig.PaperPositionMode = 'manual';
+print('displacement.png','-dpng');
+
+return;
+
+
 
 FigHandle = figure;
 set(FigHandle, 'Position', [100, 100, 1200, 300]);
